093b0a699e6eb0f0480900a5ae57a38edbd289cd
Babin Bohora <bohorababin1@gmail.com>
2022-12-06 14:09:29 +0900

    sign message function

---
+++ week-1/ecdsa-node/client/services/cryptography.js
@@ -0,0 +1,11 @@
+import { utf8ToBytes, toHex } from "ethereum-cryptography/utils";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import {sign} from "ethereum-cryptography/secp256k1";
+
+const signMessage = async (message, privateKey) => {
+  const hashedMsg = keccak256(utf8ToBytes(message));
+  const signInfo = await sign(hashedMsg, privateKey, { recovered: true });
+  return { hashedMsg, signInfo };
+};
+
+export { signMessage };
