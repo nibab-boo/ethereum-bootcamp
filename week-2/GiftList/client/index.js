7431425269c9642ac74393f86de46fcfaf061291
Babin Bohora <bohorababin1@gmail.com>
2023-01-17 15:57:00 +0900

    Sending Proof from the Client to Server

--- week-2/GiftList/client/index.js
+++ week-2/GiftList/client/index.js
@@ -4,12 +4,15 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+const merkleTree = new MerkleTree(niceList);
+
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
-
+  const name = "Ralph West";
+  const index = niceList.indexOf(name);
+  const proof = merkleTree.getProof(index);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+    name, proof, root: merkleTree.getRoot() });
 
   console.log({ gift });
 }
