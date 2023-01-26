acbec2ed5bd56870953079ba9e2fed92ce1b3a56
Babin Bohora <bohorababin1@gmail.com>
2023-01-26 13:54:59 +0900

    Signed Json-rpc req(txn) to change world state(Goerli network)

---
+++ week-3/sign-tx/index.js
@@ -0,0 +1,38 @@
+const { Alchemy, Network, Wallet, Utils } = require('alchemy-sdk');
+require('dotenv').config();
+
+const { TEST_API_KEY, TEST_PRIVATE_KEY } = process.env;
+
+const settings = {
+  apiKey: TEST_API_KEY,
+  network: Network.ETH_GOERLI,
+};
+const alchemy = new Alchemy(settings);
+
+let wallet = new Wallet(TEST_PRIVATE_KEY);
+
+async function main() {
+  const nonce = await alchemy.core.getTransactionCount(
+    wallet.address,
+    'latest'
+  );
+  console.log("Nonce :---: ", nonce);
+
+  let transaction = {
+    to: "0xd98f68967db4a9bd6c5ba0ee9c69287aeb517727",
+    value: Utils.parseEther('0.005'), // 0.005 worth of ETH being sent
+    gasLimit: '21000',
+    maxPriorityFeePerGas: Utils.parseUnits('5', 'gwei'),
+    maxFeePerGas: Utils.parseUnits('20', 'gwei'),
+    nonce: nonce,
+    type: 2,
+    chainId: 5, // göerli transaction
+  };
+
+  let rawTransaction = await wallet.signTransaction(transaction);
+  console.log('Raw tx: ', rawTransaction);
+  let tx = await alchemy.core.sendTransaction(rawTransaction);
+  console.log(`https://goerli.etherscan.io/tx/${tx.hash}`);
+}
+
+main();
