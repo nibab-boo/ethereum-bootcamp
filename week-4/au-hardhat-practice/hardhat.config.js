e43ed589b022a03fa310f3d8b5e7ca8b370e23c3
Babin Bohora <bohorababin1@gmail.com>
2023-03-08 16:59:44 +0900

    Configs and deploy code

--- week-4/au-hardhat-practice/hardhat.config.js
+++ week-4/au-hardhat-practice/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: process.env.TESTNET_ALCHEMY_URL,
+      accounts: [process.env.TESTNET_PRIVATE_KEY],
+    },
+  },
 };
